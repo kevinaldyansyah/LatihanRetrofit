c1f6f4bf7206c3b8350172b30b47f3ffa6639fbb
Kevin Aldyansyah <54729187+kevinaldyansyah@users.noreply.github.com>
2020-12-02 21:41:54 +0700

    Add files via upload

---
+++ LatihanRetrofit/app/src/main/java/com/kevin/latihanretrofit/services/GithubService.java
@@ -0,0 +1,13 @@
+package com.kevin.latihanretrofit.services;
+
+import com.kevin.latihanretrofit.models.Repo;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import java.util.List;
+
+public interface GithubService {
+    @GET("users/{user}/repos")
+    Call<List<Repo>> listRepos (@Path("user") String user);
+}
